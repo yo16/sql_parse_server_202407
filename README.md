f0fc5ab84b15cfecb49d6fde903de490eac89148
yo16 <xxyo1xx@gmail.com>
2024-07-09 11:37:49 +0900

    response node-sql-parser ast

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # sql_parse_server_202407
 SQLParserのserver
 
+# 概要
+- `node-sql-parser`を利用し、astに変換して返す
+    - [node-sql-parser - npm](https://www.npmjs.com/package/node-sql-parser)
+    - [taozhi8833998/node-sql-parser: Parse simple SQL statements into an abstract syntax tree (AST) with the visited tableList and convert it back to SQL](https://github.com/taozhi8833998/node-sql-parser)
+
 # 環境構築
 ## 参考
 - [Express(Node.js)でTypeScript環境を構築するための完全ガイド | アールエフェクト](https://reffect.co.jp/node-js/express-typescript)
--- sql_parse_ts/src/index.ts
+++ sql_parse_ts/src/index.ts
@@ -1,9 +1,81 @@
 import express from 'express';
 import type { Express, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { Parser, Option as NSPOption, AST} from 'node-sql-parser';
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
+const cors_url = process.env.CORS || "http://localhost:3000";
 
-app.get('/', (req, res) => res.send('Hello World!!'));
+app.use(
+    cors({
+        origin: cors_url,
+        credentials: true,
+        optionsSuccessStatus: 200,
+    })
+);
+app.use(
+    bodyParser.urlencoded({extended: true})
+);
+app.use(
+    bodyParser.json()
+);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// node-sql-parser instance
+const parser = new Parser();
+
+
+app.get('/', (req, res) => res.type('html').send('<!DOCTYPE html><html><body>/</body></html>'));
+app.post('/sql', (req: Request, res: Response) => {
+    try {
+        res.setHeader("Access-Control-Allow-Origin", cors_url);
+    } catch (e) {
+        console.error(e);
+    }
+
+    // bodyから情報を取り出す
+    const query = req.body.query;
+    if (!query) { res.type('json').send({})}
+    if (query.length === 0) { res.type('json').send({})}
+    let database = req.body.database;
+    if ( (!database) || (database.length === 0) ) {
+        // defaultはbigquery
+        database = 'bigquery';
+    }
+
+    // node-sql-parserを使ってastにして返す
+    let ast: AST[] = [];
+    try {
+        const opt: NSPOption = {
+            database,
+        };
+
+        // parse
+        const ret = parser.parse(query, opt);
+
+        // 戻り値の調整
+        if (Array.isArray(ret.ast)) {
+            ast = ret.ast;
+        } else {
+            ast = [ret.ast];
+        }
+        // const tableList = ret.tableList;
+        // const columnList = ret.columnList;
+
+    } catch (e) {
+        if (e instanceof Error) {
+            console.error(e.message);
+        }
+        res.type('json').send({'message': 'Could not parse.'});
+    }
+
+    res.type('json').send({
+        statements: ast,
+    });
+})
+
+
+const server = app.listen(port, () => console.log(`SqlParseServer listening on port ${port}!`));
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
